e2e44c3750361e7c110991531a3b184f696de104
pushlang <aapust@gmail.com>
2020-12-03 22:05:54 +0300

    interfaces: ...

--- ch7/u78hf/main.go
+++ ch7/u78hf/main.go
@@ -17,10 +17,17 @@ func Handle(n int, h Handler) {
 }
 
 type handlerfunc struct {
+	i int
 }
 
-func (s *handlerfunc) foo(n int) {
-	fmt.Println("handlerfunc -> foo", n)
+func (s *handlerfunc) foo1(n int) {
+	s.i += n
+	fmt.Println("handlerfunc -> foo1", n)
+}
+
+func (s *handlerfunc) foo2(n int) {
+	s.i*=n
+	fmt.Println("handlerfunc -> foo2", n)
 }
 
 type handler struct {
@@ -31,10 +38,35 @@ func (h *handler) ServeHTTP(n int) {
 }
 
 func main() {
-	var h = &handler{}
+	//var h = &handler{}
+	//Handle(2, h)
+
 	var hf = &handlerfunc{}
 
-	Handle(123, h)
+	Handle(1, HandlerFunc(hf.foo1))
+	Handle(2, HandlerFunc(hf.foo2))
+}
 
-	Handle(456, HandlerFunc(hf.foo))
+type ReaderFunc(p []byte) (n int, err error)
+
+func (f ReaderFunc) ReadSomthing(p []byte) (n int, err error) {
+	return f(p)
+}
+
+func ReadHandle(p []byte) (n int, err error) {
+	h.ReadSomthing(n)
+}
+
+type readfrom struct {
+	s string
+}
+
+func (s *readfrom) stdin(p []byte) (n int, err error) {
+	s.i += n
+	fmt.Println("handlerfunc -> foo1", n)
+}
+
+func (s *readfrom) strng(p []byte) (n int, err error) {
+	s.s=string(p)
+	return len(
 }
